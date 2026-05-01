bdea8ae330bda2ed58fa26728c2f4ecf932f5bb5
marreahsamantha <lusdocmarreahsamantha@gmail.com>
2026-05-01 15:38:35 +0800

    Add functions to create and write to a personal diary file

--- __init__.py
+++ __init__.py
@@ -1,1 +1,23 @@
+def create_file():
+    try:
+        file = open("diary.txt", "x")
+        print(" Personal Diary file created successfully!")
+        file.close()
+    except FileExistsError:
+        print(" Personal Diary file already exists.")
 
+def write_entry():
+    try:
+        date = input("Enter date: ")
+        entry = input("Write your diary entry: ")
+
+        file = open("diary.txt", "w")
+        file.write(f"[{date}] {entry}\n")
+        file.close()
+
+        print(" Diary entry saved successfully!")
+    except Exception as e:
+        print("Error writing entry:", e)
+
+create_file()
+write_entry()
